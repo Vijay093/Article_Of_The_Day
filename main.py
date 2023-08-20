31781a0832387aaa6518f077db5b2efe2ff1180e
Vijay Kumar M R <83636006+Vijay093@users.noreply.github.com>
2023-08-20 11:54:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ driver = webdriver.Chrome(options=options)
 
 driver.get('https://en.wikipedia.org/wiki/Main_Page')
 
-article_content = driver.find_element(By.CSS_SELECTOR, "#mp-tfa p")
-links = driver.find_elements(By.CSS_SELECTOR, "#mp-tfa p b a")
-link = links[-1].get_attribute('href')
+article_content = driver.find_element(By.XPATH, '//*[@id="mp-tfa"]')
+link = driver.find_element(By.PARTIAL_LINK_TEXT, 'Full article')
+link = link.get_attribute('href')
 content = article_content.text
 # print(article_content.text)
 # print(link)
+content = content[:600]+"..."
 
 with SMTP("smtp.gmail.com") as connection:
     connection.starttls()
